60d7dea98dac835a142df080c7c4f0130bfa9a7b
giuliom95 <giuliomartella1995@gmail.com>
2019-03-05 13:50:51 +0100

    Custom tree item class

--- qtapp/src/main.py
+++ qtapp/src/main.py
@@ -6,23 +6,32 @@ from PySide2 import QtCore
 
 import requests
 
+class AssetPathItem(QtWidgets.QTreeWidgetItem):
+    def getFullPath(self) -> str:
+        node = self
+        path = []
+        while node is not None:
+            path.append(node.text(0))
+            node = node.parent()
+        return '/' + '/'.join(path[::-1]) + '/'
+
+    def requestChildren(self):
+        print("Hello World: {0}".format(self.getFullPath()))
+
+
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.initUI()
 
     def initUI(self):
-        
         tree = QtWidgets.QTreeWidget()
         tree.header().hide()
-        items = [QtWidgets.QTreeWidgetItem(tree, ['{0}'.format(i)]) for i in range(10)]
-        subitems = [
-            [
-                QtWidgets.QTreeWidgetItem(item, ['{0}'.format(j)]) 
-                for j in range(20)
-            ] 
-            for item in items
-        ]
+        for i in range(10):
+            item = AssetPathItem(tree, ['{0}'.format(i)])
+            for j in range(20):
+                subitem = AssetPathItem(item, ['{0}'.format(j)])
+        tree.itemExpanded.connect(AssetPathItem.requestChildren)
 
         mainLayout = QtWidgets.QVBoxLayout()
         mainLayout.addWidget(tree)
@@ -34,6 +43,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.resize(800, 600)
         self.setWindowTitle('Test')
         self.show()
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
