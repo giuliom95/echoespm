606338b7c0e01da518fb1e27ad35af50c9e7912d
giuliom95 <giuliomartella1995@gmail.com>
2019-04-13 17:35:43 +0200

    Added responsive directory view

--- qtapp/src/main.py
+++ qtapp/src/main.py
@@ -7,7 +7,9 @@ from PySide2 import QtCore
 import requests
 import re
 
-import random
+from pathlib import Path
+
+IMAGES_PATH = Path('./qtapp/images/')
 
 def getContentDataFromServer(path):
     api_url = 'http://127.0.0.1:5000'
@@ -18,6 +20,90 @@ def getContentDataFromServer(path):
     if r.status_code / 100 != 2:
         raise RuntimeError(f'Server answered with {r.status_code}')
     return r.json()
+
+
+class ExplorerElementButton(QtWidgets.QWidget):
+
+    def __init__(self, text, pixmap):
+        super().__init__()
+        
+        self.pixmap = pixmap
+        self.text = QtWidgets.QLabel(text)
+        self.icon = QtWidgets.QLabel()
+        self.icon.setPixmap(self.pixmap.scaled(200,200))
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.icon)
+
+        layout.addWidget(self.text)
+        self.setLayout(layout)
+
+        self.setSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+
+
+class DirectoryView(QtWidgets.QScrollArea):
+
+    def __init__(self, elements):
+        super().__init__()
+        self.elements = elements
+
+        # Disable horizontal scrolling
+        self.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+
+        self.grid = QtWidgets.QGridLayout()
+        self.grid.setSpacing(1)
+
+        self.elementsPerRow = 0
+        self.displaceElements()
+
+        self.setWidgetResizable(True)
+
+        gridContainerHLayout = QtWidgets.QHBoxLayout()
+        gridContainerHLayout.setContentsMargins(0,0,0,0)
+        gridContainerHLayout.addLayout(self.grid)
+        gridContainerHLayout.addStretch()
+        gridContainerVLayout = QtWidgets.QVBoxLayout()
+        gridContainerVLayout.setContentsMargins(0,0,0,0)
+        gridContainerVLayout.addLayout(gridContainerHLayout)
+        gridContainerVLayout.addStretch()
+        gridContainer = QtWidgets.QWidget()
+        gridContainer.setLayout(gridContainerVLayout)
+        self.setWidget(gridContainer)
+        self.grid.setContentsMargins(0,0,0,0)
+
+
+    def clearGrid(self):
+        itm = self.grid.takeAt(0) 
+        while itm:
+            self.grid.removeItem(itm)
+            itm = self.grid.takeAt(0) 
+            
+
+    def displaceElements(self):
+        """
+        Fit elements into grid according to current size
+        """
+        if len(self.elements) == 0: return
+
+        elPerRow = self.width() // self.elements[0].width()
+
+        if elPerRow == 0:
+            elPerRow = 1
+
+
+        self.clearGrid()
+
+        i = 0
+        for el in self.elements:
+            print(el, i // elPerRow, i % elPerRow)
+            self.grid.addWidget(el, i // elPerRow, i % elPerRow)
+            i += 1
+
+        self.elementsPerRow = elPerRow
+
+    def resizeEvent(self, event):
+        self.displaceElements()
+
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -33,26 +119,32 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.addressBar = QtWidgets.QHBoxLayout()
         backToRootBtn = QtWidgets.QPushButton('content')
+        backToRootBtn.setFlat(True)
         self.addressBar.addWidget(backToRootBtn)
         firstAddressSlash = QtWidgets.QLabel('/')
         self.addressBar.addWidget(firstAddressSlash)
         self.addressBar.addStretch()
         mainLayout.addLayout(self.addressBar)
 
-        dirGrid = QtWidgets.QGridLayout()
-        dirGrid.addWidget(QtWidgets.QPushButton('CIAO'),0,0)
-        dirGrid.addWidget(QtWidgets.QPushButton('CIAO'),0,1)
-        dirGrid.addWidget(QtWidgets.QPushButton('CIAO'),0,2)
-        dirGrid.addWidget(QtWidgets.QPushButton('CIAO'),1,0)
-        dirGrid.addWidget(QtWidgets.QPushButton('CIAO'),1,1)
-        dirGrid.addWidget(QtWidgets.QPushButton('CIAO'),1,2)
+        pixmap = QtGui.QPixmap(str(IMAGES_PATH.joinpath('folder.png')))
 
-        dirGridContainer = QtWidgets.QWidget()
-        dirGridContainer.setLayout(dirGrid)
+        elements = [
+            ExplorerElementButton('CIAO', pixmap),
+            ExplorerElementButton('CIAO', pixmap),
+            ExplorerElementButton('CIAO', pixmap),
+            ExplorerElementButton('CIAO', pixmap),
+            ExplorerElementButton('CIAO', pixmap),
+            ExplorerElementButton('CIAO', pixmap),
+            ExplorerElementButton('CIAO', pixmap),
+            ExplorerElementButton('CIAO', pixmap),
+            ExplorerElementButton('CIAO', pixmap),
+            ExplorerElementButton('CIAO', pixmap),
+            ExplorerElementButton('CIAO', pixmap),
+            ExplorerElementButton('CIAO', pixmap)
+        ]
 
-        currentDirScrollableArea = QtWidgets.QScrollArea()
-        currentDirScrollableArea.setWidget(dirGridContainer)
-        mainLayout.addWidget(currentDirScrollableArea)
+        directoryView = DirectoryView(elements)
+        mainLayout.addWidget(directoryView)
 
         self.setCentralWidget(mainLayoutContainer)
         self.resize(800, 600)
