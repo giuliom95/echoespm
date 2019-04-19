19f6c8b401b4325cfc7015a3dc86d05b77d8558a
giuliom95 <giuliomartella1995@gmail.com>
2019-04-19 17:35:55 +0200

    Project overview HTML generation

--- src/server.py
+++ src/server.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from flask import abort
 from flask import jsonify
+from flask import render_template
 
 from db import db
 
@@ -11,7 +12,28 @@ dbconn = db()
 import re
 
 
-@app.route('/', methods=['GET'])
-def default():
-    return 'Hello world'
+@app.route('/content/<project>/', methods=['GET'])
+def project_overview(project):
+    data = dbconn.getProjectOverview(project)
+
+    # Tableify data
+    table = []
+    for ct in data:
+        row = [ct]
+        table.append(row)
+        contents = data[ct]
+        if len(contents) == 0:
+            continue
+        row += list(contents.values())[0].keys()
+        for c in contents:
+            row = [c]
+            for rt in contents[c]:
+                ver, status = contents[c][rt]
+                if ver == None:
+                    ver = 0
+                row.append(ver)
+            table.append(row)
+    
+    return render_template('project_overview.jinja2', table=table)
+    
 
